85b5d08ea28e43964eaae4286c722fc368dd7f1d
MO-ARBAZ <arbaz7860095748@gamil.com>
2023-07-27 08:28:44 +0530

    changes

---
+++ src/components/LoginPage.js
@@ -0,0 +1,41 @@
+// src/components/LoginPage.js
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/api/login', { username, password });
+      // Handle successful login, store token, etc.
+      console.log('Login successful:', response.data);
+    } catch (error) {
+      // Handle login error
+      console.log('Login error:', error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/RegisterPage.js
@@ -0,0 +1,41 @@
+// src/components/RegisterPage.js
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('/api/register', { username, password });
+      // Handle successful registration, login user, etc.
+      console.log('Registration successful:', response.data);
+    } catch (error) {
+      // Handle registration error
+      console.log('Registration error:', error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+};
+
+export default RegisterPage;
